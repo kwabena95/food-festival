6dc94c9ab48ad612b2e13c9bd998b49f22c3cc38
ebenezer <e.kankam95@hotmail.com>
2021-04-19 16:42:38 -0500

    updating

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,11 @@
+const webpack = require("webpack")
 const path = require("path");
-const webpack = require("webpack");
-
 
 module.exports = {
-    entry: './assets/js/script.js',
+    entry: "./assets/js/script.js",
     output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'main.bundle.js'
+        path: path.join(__dirname + "/dist"),
+        filename: "main.bundle.js"
     },
     plugins: [
         new webpack.ProvidePlugin({
@@ -14,5 +13,5 @@ module.exports = {
             jQuery: "jquery"
         }),
     ],
-    mode: 'development'
+    mode: "development"
 };
